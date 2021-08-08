449f64d073ad56c6666cd02668d9a9dfc614ec22
SieunJu <j.sieun73@gmail.com>
2021-08-08 17:10:59 +0900

    test: 코루틴 에러에 대한 처리를 위한 테스트 코드 수정@@2

--- server/routes/dummyRouter.js
+++ server/routes/dummyRouter.js
@@ -6,21 +6,22 @@ const utils = require('../utils/commandUtil');
 // [S] TEST
 router.get('/api/test',(req, res) => {
     try {
-        let ran = Math.floor(Math.random() * 10)
-        if(ran < 5) {
-            res.status(200).send({
-                status : true,
-                name : "Mr.Ju",
-                age : 30
-            }).end()
-        } else {
-            res.status(200).send({
-                status : true,
-                test : "dddd",
-                age : 30
-            }).end()
-        }
-        
+        setTimeout(function () {
+            let ran = Math.floor(Math.random() * 10)
+            if(ran < 5) {
+                res.status(200).send({
+                    status : true,
+                    name : "Mr.Ju",
+                    age : 30
+                }).end()
+            } else {
+                res.status(200).send({
+                    status : true,
+                    test : "dddd",
+                    age : 30
+                }).end()
+            }
+        },1500)
     } catch(err) {
         console.log("Error " + err)
     }
